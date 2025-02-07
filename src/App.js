b77d0104b4f407a59a681922007f3a52f3f8e219
Devi Sri <pallothudevisri@gmail.com>
2025-02-07 13:15:46 +0530

    login page

--- src/App.js
+++ src/App.js
@@ -2,16 +2,60 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const { setCurrentColor, setCurrentMode, currentMode, currentColor, themeSettings, setThemeSettings, activeMenu } = useStateContext();
+  const isHomePage = window.location.pathname === '/';
+  const isLoginPage = window.location.pathname === '/login';
+  const isSignupPage = window.location.pathname === '/signup';
+
+  useEffect(() => {
+    const currentThemeColor = localStorage.getItem('colorMode');
+    const currentThemeMode = localStorage.getItem('themeMode');
+    if (currentThemeColor && currentThemeMode) {
+      setCurrentColor(currentThemeColor);
+      setCurrentMode(currentThemeMode);
+    }
+  }, []);
+
+  const mainContentStyle = {
+    width: activeMenu && !isLoginPage && !isSignupPage && !isHomePage ? '85%' : '100%', // Adjust the width as needed
+    marginLeft: activeMenu && !isLoginPage && !isSignupPage && !isHomePage ? '15%' : '0',
+    transition: 'width 0.3s ease, margin-left 0.3s ease',// Add transition for smooth animation
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        
-         
-      </header>
+    <div className={currentMode === 'Dark' ? 'dark' : ''}>
+     <BrowserRouter>
+      <div className='flex relative dark:bg-main-dark-bg'>
+        <div className='fixed right-4 bottom-4' style={{zindex:"1000"}}>
+  
+        </div>
+        <div
+            style={mainContentStyle}
+            className={
+              isLoginPage || isSignupPage || isHomePage
+                ? 'w-full min-h-screen flex-2 dark:bg-main-dark-bg bg-main-bg'
+                : 'dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full'
+            }
+          >
+            <Routes>
+              <Route  path="/"
+                  element={
+                    userstate && userstate._id ? (
+                      <Profile
+                        setUserState={setUserState}
+                        username={userstate.fname}
+                      />
+                    ) : (
+                      <Login setUserState={setUserState} />
+                    )
+                  }> </Route>
+
+                <Route path="/login" element={<Login setUserState={setUserState}></Login>}></Route>
+                <Route path="/signup" element={<Register/>}></Route>
+            </Routes>
+          </div>
+      </div>
+     </BrowserRouter>
     </div>
   );
 }
---
+++ src/Components/login/login.jsx
@@ -0,0 +1,116 @@
+import React, { useState, useEffect } from "react";
+import basestyle from "../Base.module.css";
+import loginstyle from "./Login.module.css";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { useNavigate, NavLink } from "react-router-dom";
+
+
+export default function Login({setUserState})
+{
+    const navigate=useNavigate();
+    const [formErrors,setFormErrors]=useState('');
+    const [user,setUser]=useState({
+        email:"",
+        password:"",
+    });
+    const [isSubmit, setisSubmit] =useState(false);
+
+    function handleValueChange (e)
+    {
+        [id,value]=e.target();
+        setUser({
+            ...user,
+            [id]:value,
+    })
+    }
+
+    function handleSubmit()
+    {
+       e.preventDefault();
+        setFormErrors(validateForm(user));
+       setisSubmit(true);
+    }
+
+    function validateForm(values)
+    {
+        const error={};
+        const regex = /^[^\s+@]+@[^\s@]+\.[^\s@]{2,}$/i;
+        if(!values.email)
+        {
+            error.email("Email is required");
+        }
+        else if (regex.test(values.email))
+        {
+            error.email("Enter valid email");
+        }
+        if(!values.password)
+        {
+            error.password("Password is required");
+        }
+        return error;
+    }
+    useEffect(()=>
+    {
+        if(Object.keys(formErrors).length==0 && isSubmit)
+        {
+            const Admin = { email: "Admin@gmail.com", password: "admin123" };
+            const employee = { email: "employee@gmail.com", password: "employee123" };
+            const developer = { email: "developer@gmail.com", password: "developer123" };
+
+            if(user.email === Admin.email && user.password === Admin.password)
+            {
+                toast.success("Login Successfull");
+                 setUserState(Admin);
+                 navigate("/home",{replace:true})
+            }
+            if(user.email === employee.email && user.password === employee.password)
+            {
+                setUserState(employee);
+                toast.success("Login Successfull");
+                navigate("/home",{replace:true})
+            }
+            if(user.email === developer.email && user.password === developer.password)
+            {
+                toast.success("Login Successfull");
+                setUserState(developer);
+                navigate("/home",{replace:true})
+            }
+            else
+            {
+                toast.failure("Login failed");
+            }
+
+        }
+    },[formErrors,isSubmit])
+    return(
+    <div  className={`${loginstyle.loginPageContainer}`} >
+        <div className={loginstyle.loginFormContainer}>
+        <h1>Login</h1>
+        <form>
+            <input name="email"
+            type="email"
+            id="email"
+            placeholder="Email"
+            value={user.email}
+            onChange={handleValueChange}
+            className={loginstyle.loginFormInput}
+            >
+            </input>
+            <p className={basestyle.error}>{formErrors.email}</p>
+            <input name="password"
+            type="password"
+            id="password"
+            placeholder="Password"
+            value={user.password}
+            onChange={handleValueChange}
+            className={loginstyle.loginFormInput}
+            >
+            <p className={basestyle.error}>{formErrors.password}</p>
+            </input>
+            <button className={`${basestyle.button_common} `} onClick={handleSubmit}> Submit</button>
+            <NavLink to="/signup">Not yet registered? Register Now</NavLink>
+        </form>
+        </div>
+    </div>);
+}
