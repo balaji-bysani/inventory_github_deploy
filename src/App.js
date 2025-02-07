979cad61e639be8eae84bb0e52412fe24ac422be
Devi Sri <pallothudevisri@gmail.com>
2025-02-07 13:43:45 +0530

    Debugging app.js

--- src/App.js
+++ src/App.js
@@ -20,41 +20,15 @@ function App() {
     }
   }, []);
 
-  const mainContentStyle = {
-    width: activeMenu && !isLoginPage && !isSignupPage && !isHomePage ? '85%' : '100%', // Adjust the width as needed
-    marginLeft: activeMenu && !isLoginPage && !isSignupPage && !isHomePage ? '15%' : '0',
-    transition: 'width 0.3s ease, margin-left 0.3s ease',// Add transition for smooth animation
-  };
+  // const mainContentStyle = {
+  //   width: activeMenu && !isLoginPage && !isSignupPage && !isHomePage ? '85%' : '100%', // Adjust the width as needed
+  //   marginLeft: activeMenu && !isLoginPage && !isSignupPage && !isHomePage ? '15%' : '0',
+  //   transition: 'width 0.3s ease, margin-left 0.3s ease',// Add transition for smooth animation
+  // };
 
   return (
     <div className={currentMode === 'Dark' ? 'dark' : ''}>
-     <BrowserRouter>
-      <div className='flex relative dark:bg-main-dark-bg'>
-        <div className='fixed right-4 bottom-4' style={{zindex:"1000"}}>
-  
-        </div>
-        <div
-            style={mainContentStyle}
-            className={
-              isLoginPage || isSignupPage || isHomePage
-                ? 'w-full min-h-screen flex-2 dark:bg-main-dark-bg bg-main-bg'
-                : 'dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full'
-            }
-          >
-            <Routes>
-              <Route  path="/"
-                  element={
-                    (
-                      <Login setUserState={setUserState} />
-                    )
-                  }> </Route>
-
-                <Route path="/login" element={<Login setUserState={setUserState}></Login>}></Route>
-                
-            </Routes>
-          </div>
-      </div>
-     </BrowserRouter>
+    <Login setUserState={setUserState} />
     </div>
   );
 }
